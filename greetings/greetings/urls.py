0ccea9de1a45a14fa4586a47e0b420432fd73a2a
Rohit <rohit.shivaji.shinde@gmail.com>
2021-02-03 10:26:14 +0530

    update names

--- greetings/greetings/urls.py
+++ greetings/greetings/urls.py
@@ -1,4 +1,4 @@
-"""temp URL Configuration
+"""greetings URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    url('temp', include('tempapp.urls'))
+    url('greet', include('greet.urls'))
 ]
